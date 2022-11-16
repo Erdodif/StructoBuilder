3a5192b7601ceff55121a83954c0d4996d5ded8b
Erdodif <erdodif@gmail.com>
2022-11-16 11:42:58 +0100

    improved conversion in Deserializer

--- src/StructogramController.ts
+++ src/StructogramController.ts
@@ -1,5 +1,5 @@
 import "./Statement.js";
-import { StatementType, Statement, StatementDeSerializer, LoopStatement, IfStatement, I_Statement } from "./Statement.js";
+import { StatementType, Statement, StatementDeSerializer, LoopStatement, IfStatement, I_Statement, SwitchStatement, ReversedLoopStatement } from "./Statement.js";
 export class StructogramController {
     structogram: Structogram;
 
@@ -16,30 +16,40 @@ export class StructogramController {
         return mapping.replace(/[^;0-9]/, "").split(";").map(Number);
     }
 
-    protected getSubElement(statement: Statement | Statement[], index: number): Statement[] | Statement | null {
-        if (Array.isArray(statement)) {
-            return statement[index];
+    static getSubElement(holder: Statement | Statement[] | Structogram | null, index: number):
+        Statement[] | Statement | null {
+        if (!holder) {
+            return null;
         }
-        switch (statement.type()) {
+        if (holder instanceof Structogram) {
+            return holder.statements[index];
+        }
+        if (Array.isArray(holder)) {
+            return holder[index];
+        }
+        switch (holder.type()) {
             case StatementType.S_IF:
+                return (holder as IfStatement).statementBlocks[index] as Statement[];
             case StatementType.S_SWITCH:
-                return (statement as IfStatement).statementBlocks[index];
+                return (holder as SwitchStatement).blocks[index].statements as Statement[];
             case StatementType.S_LOOP:
+                return (holder as ReversedLoopStatement).statements[index] as Statement;
             case StatementType.S_LOOP_REVERSE:
-                return (statement as LoopStatement).statements[index];
+                return (holder as LoopStatement).statements[index] as Statement;
             default:
                 return null;
         }
     }
 
-    getElementByMapping(mapping: number[]): Statement | null {
+    getElementByMapping(mapping: number[])
+        : Statement[] | Statement | IfStatement | SwitchStatement | LoopStatement | ReversedLoopStatement | null {
         if (mapping.length < 1) {
             return null;
         }
         let current: Statement | Statement[] | null = this.structogram.statements[mapping[0]];
         let i: number = 1;
         while (i < mapping.length && current !== null) {
-            current = this.getSubElement(current, mapping[i]);
+            current = StructogramController.getSubElement(current, mapping[i]);
             i++;
         }
         return current as Statement | null;
@@ -50,7 +60,7 @@ export class StructogramController {
     }
 }
 
-export interface I_Structogram{signature: string | null | undefined, renderStart: boolean | undefined, statements: I_Statement[] };
+export interface I_Structogram { signature: string | null | undefined, renderStart: boolean | undefined, statements: I_Statement[] };
 export class Structogram {
     name: string | null;
     statements: Statement[];
@@ -65,7 +75,7 @@ export class Structogram {
         let structogram = new Structogram(json.signature);
         structogram.renderStart = json?.renderStart === true;
         let statements: Statement[] = [];
-        for(let i = 0; i <json.statements.length; i++){
+        for (let i = 0; i < json.statements.length; i++) {
             statements.push(StatementDeSerializer.fromJson(json.statements[i]));
             //TODO: Not Workin' F
         }
